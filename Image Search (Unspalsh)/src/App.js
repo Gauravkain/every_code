dad62addf825cf8ccdabd7a77b1888be97cd2313
Gaurav Kumar <55948253+Gauravkain@users.noreply.github.com>
2021-08-15 01:23:26 +0530

    Add files via upload

---
+++ Image Search (Unspalsh)/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Search from './Search';
+import Sresult from './Sresult';
+
+const App = () => {
+  return (
+    <>
+    <Search />
+    <Sresult />
+    </>
+  )
+};
+
+export default App;
